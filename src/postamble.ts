89b6f8ae6f6371e53e0967a7e772d1edd6bda499
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2022-10-29 20:54:51 +0100

    feat: named queries

--- src/postamble.ts
+++ src/postamble.ts
@@ -3,15 +3,35 @@ export const postamble = (operations: string[]) =>
     .map(
       op => `
 export function ${op}<Sel extends Selection<$RootTypes.${op}>>(
+  name: string,
   selectFn: (q: $RootTypes.${op}) => [...Sel]
-) {
+): TypedDocumentNode<GetOutput<Sel>, GetVariables<Sel>>
+export function ${op}<Sel extends Selection<$RootTypes.${op}>>(
+  selectFn: (q: $RootTypes.${op}) => [...Sel]
+): TypedDocumentNode<GetOutput<Sel>, GetVariables<Sel>>
+export function ${op}<Sel extends Selection<$RootTypes.query>>(name: any, selectFn?: any) {
+  if (!selectFn) {
+    selectFn = name
+    name = ''
+  }
   let field = new $Field<'${op}', GetOutput<Sel>, GetVariables<Sel>>('${op}', {
     selection: selectFn(new $Root.${op}()),
   })
-  const str = fieldToQuery('${op}', field)
+  const str = fieldToQuery(\`${op} \${name}\`, field)
 
-  return gql(str) as any as TypedDocumentNode<GetOutput<Sel>, GetVariables<Sel>>
+  return gql(str) as any
 }
 `
+      // `
+      // export function ${op}<Sel extends Selection<$RootTypes.${op}>>(
+      //   selectFn: (q: $RootTypes.${op}) => [...Sel]
+      // ) {
+      //   let field = new $Field<'${op}', GetOutput<Sel>, GetVariables<Sel>>('${op}', {
+      //     selection: selectFn(new $Root.${op}()),
+      //   })
+      //   const str = fieldToQuery('${op}', field)
+
+      //   return gql(str) as any as TypedDocumentNode<GetOutput<Sel>, GetVariables<Sel>>
+      // }`
     )
     .join('\n')
