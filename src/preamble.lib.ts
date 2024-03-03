f6c6ba66658ae2b300969f722a0eba2c3eeb9f22
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2024-03-03 00:15:09 +0000

    fix: parcel regressions

--- src/preamble.lib.ts
+++ src/preamble.lib.ts
@@ -1,7 +1,10 @@
-// @ts-ignore
+// The ignore pragmas are needed for the parcel types transformer, which
+// doesn't understand the fs module.
+
+//@ts-ignore
 import * as fs from 'fs'
-declare let __dirname: string
 
 export const Preamble = fs
+  //@ts-ignore
   .readFileSync(__dirname + '/preamble.src.ts', 'utf8')
   .split('/* BEGIN PREAMBLE */')[1]!
